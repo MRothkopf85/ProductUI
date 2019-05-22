25b5ad4a2b7a452dff10d3a83fd88c494a4bc738
Matt Rothkopf <mattrothkopf@localhost.localdomain>
2019-05-22 12:04:36 -0400

    First commit

---
+++ assets/js/newProducts.js
@@ -0,0 +1,374 @@
+var items = [
+  {
+    "name": "Handcrafted Frozen Bacon",
+    "brand": null,
+    "description": "Harum dignissimos placeat fugiat et qui. Accusantium velit impedit omnis. Ipsum voluptatem dicta voluptatem nobis.",
+    "shortDescription": "Harum dignissimos placeat fugiat et qui. Accusanti...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$33.00",
+    "prodId": "58ed390b-e4be-48b2-862c-59a27d619481",
+    "rating": "4.0",
+    "ratingCount": 386,
+    "salePrice": null
+  },
+  {
+    "name": "Licensed Fresh Keyboard",
+    "brand": "Mertz - Welch",
+    "description": "Occaecati laborum voluptates. Voluptatem natus sunt suscipit ullam. Fugit beatae eum at quae odio. Quia unde minus qui eos velit voluptas vero. Aliquid enim facere mollitia exercitationem officiis rerum voluptatibus voluptas. Dolorem debitis reprehenderit exercitationem explicabo magni.",
+    "shortDescription": "Occaecati laborum voluptates. Voluptatem natus sun...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$78.00",
+    "prodId": "6f579d24-296e-4a85-9008-5211435ac303",
+    "rating": "3.0",
+    "ratingCount": 301,
+    "salePrice": null
+  },
+  {
+    "name": "Fantastic Granite Fish",
+    "brand": null,
+    "description": "Ut qui ipsa. Aut iste voluptas cupiditate. Aliquid esse nihil id qui excepturi dolores mollitia id voluptatem. Ullam molestias accusantium eaque corporis. Et alias iste nam error.",
+    "shortDescription": "Ut qui ipsa. Aut iste voluptas cupiditate. Aliquid...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$26.00",
+    "prodId": "216fd5f7-7f17-455d-8791-dd1579c1b89b",
+    "rating": "1.0",
+    "ratingCount": 157,
+    "salePrice": "$10.00"
+  },
+  {
+    "name": "Small Soft Gloves",
+    "brand": null,
+    "description": "Quos excepturi doloremque tempore mollitia. Quia ab sed laboriosam. Odit veritatis dolor et rerum dolore commodi voluptas eius.",
+    "shortDescription": "Quos excepturi doloremque tempore mollitia. Quia a...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$59.00",
+    "prodId": "fb37b9f2-d2ba-4308-bf8b-3e6ae5e19382",
+    "rating": "1.0",
+    "ratingCount": 73,
+    "salePrice": "$58.00"
+  },
+  {
+    "name": "Rustic Cotton Pants",
+    "brand": null,
+    "description": "Dolorem natus eaque debitis illum qui vel impedit amet. Quis molestiae maxime eligendi. Odit esse odio incidunt. Et molestias voluptas illum. Sunt non et sapiente error voluptatem officia et velit.",
+    "shortDescription": "Dolorem natus eaque debitis illum qui vel impedit ...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$60.00",
+    "prodId": "a0e087ab-1836-4520-9e93-c9d993c7980d",
+    "rating": "4.0",
+    "ratingCount": 173,
+    "salePrice": "$11.00"
+  },
+  {
+    "name": "Handmade Wooden Shirt",
+    "brand": "Blick - Spinka",
+    "description": "Sit atque velit nulla eveniet quia repellendus. Nobis pariatur voluptas. Voluptate blanditiis sunt esse alias sed placeat laboriosam.",
+    "shortDescription": "Sit atque velit nulla eveniet quia repellendus. No...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$49.00",
+    "prodId": "3c924ca9-ce56-411d-acb6-6d4624ed4b78",
+    "rating": "4.0",
+    "ratingCount": 80,
+    "salePrice": "$18.00"
+  },
+  {
+    "name": "Fantastic Metal Shirt",
+    "brand": null,
+    "description": "Delectus dolore ipsam. Et aut enim sit accusantium ex. Corporis sit est. Ut quaerat nisi. Ex praesentium voluptatem magni perspiciatis consequatur aliquam ea nemo accusantium. Vero qui ad.",
+    "shortDescription": "Delectus dolore ipsam. Et aut enim sit accusantium...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$47.00",
+    "prodId": "8c9d9818-f156-4466-80e0-4364c06628db",
+    "rating": "3.0",
+    "ratingCount": 106,
+    "salePrice": "$3.00"
+  },
+  {
+    "name": "Tasty Fresh Chicken",
+    "brand": null,
+    "description": "Ut et ipsam consequatur. Et enim et ab qui harum quis vel ad tempora. Labore itaque dolore impedit distinctio voluptatem ut. Eum praesentium qui.",
+    "shortDescription": "Ut et ipsam consequatur. Et enim et ab qui harum q...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$99.00",
+    "prodId": "3e0efcd0-d4f3-496e-b2c1-b50c95da0058",
+    "rating": "4.0",
+    "ratingCount": 77,
+    "salePrice": null
+  },
+  {
+    "name": "Generic Metal Computer",
+    "brand": null,
+    "description": "Quam eum accusamus qui odit eveniet sunt inventore animi ea. Magni corporis dicta. Repellendus molestiae corporis voluptas qui labore. Deserunt aut numquam animi voluptas facilis et inventore est ullam.",
+    "shortDescription": "Quam eum accusamus qui odit eveniet sunt inventore...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$91.00",
+    "prodId": "ac28fcc2-e1c7-4882-b339-acc805faf685",
+    "rating": "1.0",
+    "ratingCount": 163,
+    "salePrice": "$32.00"
+  },
+  {
+    "name": "Practical Fresh Hat",
+    "brand": "Shields - Christiansen",
+    "description": "Autem quia cumque qui perspiciatis cumque dignissimos inventore ullam. Est excepturi est asperiores accusamus. Deserunt ducimus voluptatem animi. Aspernatur nesciunt quis distinctio quisquam quia omnis atque quo. Culpa deleniti sit qui et qui rerum vel. Est fuga tempore quas voluptatem voluptates ad.",
+    "shortDescription": "Autem quia cumque qui perspiciatis cumque dignissi...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$6.00",
+    "prodId": "aa8ed9ad-b33e-4e02-b222-e5e0921b1dc0",
+    "rating": "4.0",
+    "ratingCount": 279,
+    "salePrice": null
+  },
+  {
+    "name": "Small Concrete Chicken",
+    "brand": "O'Hara - Bradtke",
+    "description": "Est dolores magni nam sed. Facere voluptas enim neque similique. Voluptatum quod iste. Odio quos eligendi eos porro molestias possimus in.",
+    "shortDescription": "Est dolores magni nam sed. Facere voluptas enim ne...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$15.00",
+    "prodId": "190dab55-e1a0-498f-ad4a-34052404fe54",
+    "rating": "5.0",
+    "ratingCount": 43,
+    "salePrice": "$7.00"
+  },
+  {
+    "name": "Rustic Rubber Table",
+    "brand": "Franecki, Vandervort and Mertz",
+    "description": "Aperiam est numquam tempore. Nihil et at consectetur occaecati voluptas dignissimos eveniet. Libero et eius temporibus iusto saepe similique. Aut dolores non placeat odit. Doloremque repellendus eum et minus quo magni. Nihil magnam nisi inventore voluptate sunt aut.",
+    "shortDescription": "Aperiam est numquam tempore. Nihil et at consectet...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$29.00",
+    "prodId": "9740afa1-87fc-4c4e-a9df-8bf0b43d27b2",
+    "rating": "4.0",
+    "ratingCount": 184,
+    "salePrice": null
+  },
+  {
+    "name": "Sleek Fresh Bike",
+    "brand": "Corkery, Hickle and Schamberger",
+    "description": "Fugit et et quis est quidem non consequatur. Ut blanditiis omnis maxime voluptas dolores odit vel. Ab facilis voluptas sequi ipsa alias odit. Accusamus consequatur dolorum ad neque minima ut quia quis deleniti. Recusandae in repudiandae quis et ullam qui molestiae ut. Ad perferendis itaque nesciunt assumenda.",
+    "shortDescription": "Fugit et et quis est quidem non consequatur. Ut bl...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$81.00",
+    "prodId": "1e0dbb14-3523-4b12-bb9e-e3b99d498ac4",
+    "rating": "2.0",
+    "ratingCount": 399,
+    "salePrice": null
+  },
+  {
+    "name": "Unbranded Rubber Shirt",
+    "brand": "McClure Inc",
+    "description": "Maxime non eaque impedit et pariatur fugiat dolorem perferendis qui. Eaque deleniti repellat doloremque omnis aut et. Omnis veritatis quibusdam mollitia sed.",
+    "shortDescription": "Maxime non eaque impedit et pariatur fugiat dolore...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$73.00",
+    "prodId": "c1ccace0-7d7e-4a6d-8bd1-9f2a3bdcde30",
+    "rating": "5.0",
+    "ratingCount": 91,
+    "salePrice": null
+  },
+  {
+    "name": "Sleek Cotton Shirt",
+    "brand": null,
+    "description": "Cupiditate deleniti rerum architecto consequatur voluptatem accusantium. Est optio ipsum tempora eligendi. Quis vel illo corrupti culpa quia. Vel quia minima delectus quidem. Illum ut quidem aut.",
+    "shortDescription": "Cupiditate deleniti rerum architecto consequatur v...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$65.00",
+    "prodId": "f8bb1011-304e-45f6-8739-e6a9c58ec65a",
+    "rating": "1.0",
+    "ratingCount": 268,
+    "salePrice": null
+  },
+  {
+    "name": "Practical Cotton Chips",
+    "brand": "O'Connell, Ryan and Friesen",
+    "description": "Architecto at magni. Earum animi corporis incidunt numquam. Aliquam recusandae explicabo.",
+    "shortDescription": "Architecto at magni. Earum animi corporis incidunt...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$25.00",
+    "prodId": "f3474d1a-5cc6-4967-b35b-821e9640b346",
+    "rating": "4.0",
+    "ratingCount": 453,
+    "salePrice": "$6.00"
+  },
+  {
+    "name": "Intelligent Metal Shoes",
+    "brand": null,
+    "description": "Minus exercitationem eos adipisci. Unde inventore non delectus omnis ea atque qui. Non dolorem vel molestiae velit porro voluptatum sequi.",
+    "shortDescription": "Minus exercitationem eos adipisci. Unde inventore ...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$5.00",
+    "prodId": "bdf3bfb8-b303-41a8-a445-dd67df49f6d8",
+    "rating": "3.0",
+    "ratingCount": 217,
+    "salePrice": null
+  },
+  {
+    "name": "Generic Plastic Computer",
+    "brand": null,
+    "description": "Doloribus asperiores soluta. Consectetur aut quae. Perferendis molestiae distinctio sit odit.",
+    "shortDescription": "Doloribus asperiores soluta. Consectetur aut quae....",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$93.00",
+    "prodId": "d14fdfee-8cb1-47b4-ba2c-a30dc8e80452",
+    "rating": "5.0",
+    "ratingCount": 168,
+    "salePrice": "$11.00"
+  },
+  {
+    "name": "Ergonomic Soft Keyboard",
+    "brand": null,
+    "description": "Iusto dolore sunt magnam necessitatibus nesciunt temporibus ad numquam. Omnis quaerat officia consequatur quis ut. Sit autem corporis aperiam ipsam vel enim ea ut quaerat. Animi eveniet est quis qui est sint enim officiis consequatur. Aut facere autem aliquam aut. Vel rerum dolor maxime rerum sint quia aperiam.",
+    "shortDescription": "Iusto dolore sunt magnam necessitatibus nesciunt t...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$7.00",
+    "prodId": "adb23bc9-eaf2-4f88-9b41-de4d655fc6f0",
+    "rating": "4.0",
+    "ratingCount": 120,
+    "salePrice": null
+  },
+  {
+    "name": "Handcrafted Granite Cheese",
+    "brand": null,
+    "description": "Accusamus accusantium in consequatur sequi quam. Ea animi repellat sint nesciunt molestiae iusto. Enim saepe ut aut officia molestias nihil itaque.",
+    "shortDescription": "Accusamus accusantium in consequatur sequi quam. E...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$57.00",
+    "prodId": "e0423b9e-30b7-4a13-a295-f5991c8518b9",
+    "rating": "1.0",
+    "ratingCount": 486,
+    "salePrice": null
+  },
+  {
+    "name": "Intelligent Frozen Cheese",
+    "brand": "Ledner, Gulgowski and Swift",
+    "description": "Et est voluptatum quas dignissimos et. Porro rerum perferendis quia. Deserunt in quia. Ut natus rem id aspernatur eius sed sint perspiciatis. Aut est ut autem. Non vel qui distinctio fugit voluptas est vel atque.",
+    "shortDescription": "Et est voluptatum quas dignissimos et. Porro rerum...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$73.00",
+    "prodId": "7a35e0f2-3c0d-49a2-91eb-bc7abdae0bc6",
+    "rating": "4.0",
+    "ratingCount": 371,
+    "salePrice": null
+  },
+  {
+    "name": "Rustic Steel Gloves",
+    "brand": null,
+    "description": "Ut laudantium doloribus. Officia soluta velit ex doloribus placeat at qui nisi. Expedita autem recusandae voluptatibus natus. Et molestiae totam sint asperiores ut recusandae. Doloremque aliquam ducimus facilis laboriosam praesentium.",
+    "shortDescription": "Ut laudantium doloribus. Officia soluta velit ex d...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$16.00",
+    "prodId": "b7774de2-b44e-4caf-bb97-7c8889dfafd9",
+    "rating": "2.0",
+    "ratingCount": 193,
+    "salePrice": null
+  },
+  {
+    "name": "Unbranded Fresh Computer",
+    "brand": null,
+    "description": "Doloribus sit aliquam id ad explicabo. Fugit aut ut consequatur tenetur ex vel dolores maiores rerum. Commodi molestiae architecto sit et voluptas minus id.",
+    "shortDescription": "Doloribus sit aliquam id ad explicabo. Fugit aut u...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$52.00",
+    "prodId": "05f69c03-80a6-49e4-adb6-fa442b5b4f43",
+    "rating": "3.0",
+    "ratingCount": 36,
+    "salePrice": "$30.00"
+  },
+  {
+    "name": "Licensed Wooden Bike",
+    "brand": null,
+    "description": "Assumenda dolores id omnis alias vitae illo voluptas quod. Dolorem et consequatur possimus id. Molestiae ducimus consequatur. Et et consequatur totam officiis eum sequi libero sed voluptatem. Ea voluptas molestiae atque aut. A blanditiis modi.",
+    "shortDescription": "Assumenda dolores id omnis alias vitae illo volupt...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$5.00",
+    "prodId": "77e42f67-db9f-4567-850d-7bac183fbabd",
+    "rating": "5.0",
+    "ratingCount": 204,
+    "salePrice": null
+  },
+  {
+    "name": "Unbranded Frozen Bacon",
+    "brand": "Eichmann, Schroeder and Cummerata",
+    "description": "Deserunt est soluta nobis impedit placeat quia iure eum quia. Enim iure rerum amet necessitatibus nihil. Id laudantium doloribus aspernatur quasi consectetur at voluptate. Molestiae deleniti neque ullam ad atque ut reprehenderit quibusdam. Delectus minima consectetur explicabo iusto quas est dolor ipsam incidunt. Optio saepe rerum vitae.",
+    "shortDescription": "Deserunt est soluta nobis impedit placeat quia iur...",
+    "imageUrl": "https://placebeard.it/400x400",
+    "price": "$43.00",
+    "prodId": "599d26dc-fb47-41bb-ae3d-f09676e632ac",
+    "rating": "5.0",
+    "ratingCount": 366,
+    "salePrice": "$24.00"
+  }
+];
+
+
+
+// document.body.onload = addElement;
+
+// function addElement () {
+    for(i = 0; i < items.length; i++) {
+        //Create elements for items and pop windows, and html for individual item details
+        var prodDiv = document.createElement("div");
+        var popDiv = document.createElement("div");
+        var divImage = "<img class='img-fluid' src=" + items[i].imageUrl + ">";
+        var divName = "<h3>" + items[i].name + "</h3>";
+        var divBrand = "<h6>Brand: " + items[i].brand + "</h6>";
+        var divShort = "<h6>" + items[i].shortDescription + "</h6></div>";
+        var divLong = "<p>" + items[i].description + "</p></div>";
+        var divPrice = "<h4>" + items[i].price + "</h4>";
+        var divSale = "<h4 id='sale'>SALE!!  " + items[i].salePrice + "</h4>";
+        var rating =  "<p> RATING: " + items[i].rating + " <em>(" + items[i].ratingCount + ")</em></p>";
+        var prodId = "<p> Product ID: " + items[i].prodId + "</p>";
+        var dtlButton = "<button class='open'>Details</button>";
+        var itemId = "itemId-" + i;
+
+
+        var popUpWindow = "<div class='popup-content'>" + divName + divPrice + divBrand + rating
+         + prodId + divLong + "</p><button class='close'>Close</button></div>";
+
+        var blob = divImage + divName + divPrice + divBrand + divShort;
+
+
+        //Account for null values
+        if(items[i].brand == null) {
+            if(items[i].salePrice == null) {
+                var blob = divImage + divName + divPrice + divShort + dtlButton;
+            } else {
+                var blob = divImage + divName + divSale + divShort + dtlButton;
+            }
+        } else {
+            if(items[i].salePrice == null) {
+                var blob = divImage + divName + divPrice + divBrand + divShort + dtlButton;
+            } else {
+            var blob = divImage + divName + divSale + divBrand + divShort + dtlButton;
+        }
+
+        }
+
+        //Add item info to HTML element
+        prodDiv.innerHTML = blob;
+
+        popDiv.innerHTML = popUpWindow;
+
+        //Add class attributes
+        prodDiv.setAttribute("class", "col-sm-4 col-lg-2 prodContent");
+        popDiv.setAttribute("class", "popup-overlay");
+
+        //Add new element to div
+        var currentDiv = document.getElementById("allDiv");
+        currentDiv.appendChild(prodDiv);
+        prodDiv.appendChild(popDiv);
+
+    };
+
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+$(".prodContent button").on("click", function(){
+    $(this).parent().children().addClass("active");
+    $(this).parent().children().children().addClass("active");
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+$(".prodContent").on("click", ".close", function(){
+    $(this).parent().removeClass("active");
+    $(this).siblings().removeClass("active");
+});
